69ab86b32c4862e0f91bcd54b7971a6061b65702
KrySonita <nitafromyt@gmail.com>
2025-02-07 17:36:28 +0700

    Initial Commit

---
+++ kick_air/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kick_air
+//
+//  Created by Kry Sonita on 07/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kick_air/kick_airApp.swift
@@ -0,0 +1,17 @@
+//
+//  kick_airApp.swift
+//  kick_air
+//
+//  Created by Kry Sonita on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct kick_airApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
